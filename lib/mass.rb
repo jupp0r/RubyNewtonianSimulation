4ee23b177bd08218e6ef66faea4226bf032a8d22
Jupp Müller <jupp0r@gmail.com>
2012-02-19 23:42:43 +0100

    some refactoring

--- lib/mass.rb
+++ lib/mass.rb
@@ -3,70 +3,52 @@ require 'matrix'
 # This class represents one object/mass
 # at the moment we support only simple point-masses
 class Mass
+
+attr_accessor :mass, :move, :position, :forces, :acceleration
   
   def initialize(m, ar, pos)
-    @mass = 0.0
-    @move = Vector[0.0, 0.0, 0.0]
     @movedb = Vector[0.0, 0.0, 0.0]
-    @position = Vector[0.0, 0.0, 0.0]
-    @positiondb = Vector[0.0, 0.0, 0.0]
     @forces = Vector[0.0, 0.0, 0.0]
+    @positiondb = Vector[0.0, 0.0, 0.0]
     @acceleration = Vector[0.0, 0.0, 0.0]
     @mass = m
     @move = ar
     @position = pos
-  end
-  
-  def setposition(pos)
-    @positiondb = pos
-  end
-  
-  def setmove(mov)
-    @movedb = mov
-  end
-  
-  def getmove
-    return @move
-  end
-  
-  def getmass
-    return @mass
-  end
-  
-  def getposition
-    return @position
-  end
-  
-  def getforces
-    return @forces
+    @@g = 6.67384*10**(-11)
   end
   
   def calcforces(others)
     sumf = Vector[0.0, 0.0, 0.0]
-    g = 6.67384*10**(-11)
     
     puts "mass.rb - others.inspect: " + others.inspect
     
     others.each_value do |i|
-      a = Math.sqrt((i.getposition[0] - @position[0]) + 
-          (i.getposition[1] - @position[1]) + 
-          (i.getposition[2] - @position[2]))**3
+      a = Math.sqrt((i.position[0] - @position[0]) + 
+          (i.position[1] - @position[1]) + 
+          (i.position[2] - @position[2]))**3
       
-      sumf += i.getmass * ((i.getposition - @position) * (1.0/a))
+      sumf += i.mass * ((i.position - @position) * (1.0/a))
     end
     
-    @forces = g * sumf
+    @forces = @@g * sumf
   end
   
   def calcaccel
     @acceleration = @forces / @mass
+  end
+
+  def position= (pos)
+    @positiondb = pos
+  end
+
+  def move= (m)
+      @movedb = m
   end
   
   # updates all values from double-buffer to values
   def update
     @move = @movedb
     @position = @positiondb
-    
   end
   
 end
